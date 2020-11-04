ef68db156af53969cd5aacda73b2eda95c17ded7
Mansi Kalra <54201347+mansikalra23@users.noreply.github.com>
2020-11-04 17:57:18 +0530

    Update main.go

--- SQL/main.go
+++ SQL/main.go
@@ -10,7 +10,7 @@ import (
 func main() {
 	fmt.Println(" Go and MySQL")
 
-	db, err := sql.Open("mysql", "root:shivamansi@tcp(127.0.0.1:3306)/testdb")
+	db, err := sql.Open("mysql", "root:password@tcp(127.0.0.1:3306)/testdb")
 
 	if err != nil {
 		panic(err.Error())
